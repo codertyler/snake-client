e85e5b5cc4b72f8eaed7cabb69f76b9f95da8b91
codertyler <codertylerasai@gmail.com>
2020-09-23 17:22:04 +0000

    error handler commit

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: "10.2",
+    port: 50541
+  },);
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  //Error handler
+  conn.on('error', (error) => {
+    console.log("you ded cuz you idled");
+  })
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
